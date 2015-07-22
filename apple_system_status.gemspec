f1f233486dfddf223d91254dfaaf243d00a48089
sue445 <sue445@sue445.net>
2015-07-23 01:07:18 +0900

    Impl cli

--- apple_system_status.gemspec
+++ apple_system_status.gemspec
@@ -29,6 +29,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "capybara"
   spec.add_dependency "poltergeist"
+  spec.add_dependency "thor"
 
   spec.add_development_dependency "activesupport"
   spec.add_development_dependency "bundler", "~> 1.10"
---
+++ spec/apple_system_status_spec.rb
@@ -0,0 +1,37 @@
+describe AppleSystemStatus do
+  describe "#format_response" do
+    subject { AppleSystemStatus.format_response(hash, format) }
+
+    let(:hash) do
+      {
+        title: "System Status as of 1:07 AM JST",
+        statuses: [
+          { title: "App Store", description: "No Issues: App Store", status: "allgood" },
+        ]
+      }
+    end
+
+    context "plain format" do
+      let(:format) { "plain" }
+      let(:expected) do
+        <<-EOS.strip_heredoc
+          System Status as of 1:07 AM JST
+          App Store,allgood,No Issues: App Store
+        EOS
+      end
+
+      it { should eq expected }
+    end
+
+    context "json format" do
+      let(:format) { "json" }
+      let(:expected) do
+        <<-JSON.strip_heredoc.strip
+          {"title":"System Status as of 1:07 AM JST","statuses":[{"title":"App Store","description":"No Issues: App Store","status":"allgood"}]}
+        JSON
+      end
+
+      it { should eq expected }
+    end
+  end
+end
