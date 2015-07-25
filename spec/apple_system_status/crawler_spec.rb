738ed96d34e1734982500c8435227167c90e5a32
sue445 <sue445@sue445.net>
2015-07-26 02:11:17 +0900

    Fix error when exists blank cell

--- spec/apple_system_status/crawler_spec.rb
+++ spec/apple_system_status/crawler_spec.rb
@@ -27,5 +27,19 @@ describe AppleSystemStatus::Crawler do
         end
       end
     end
+
+    context "has country (with blank cell)" do
+      let(:country) { "tw" }
+
+      it "should return system statuses" do
+        actual = crawler.perform(country)
+
+        aggregate_failures do
+          expect(actual[:title]).not_to be_blank
+          expect(actual[:statuses]).not_to be_empty
+          expect(actual[:statuses]).to all(be_a_system_status)
+        end
+      end
+    end
   end
 end
