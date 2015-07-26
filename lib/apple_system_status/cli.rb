1aceaf49b584f69916e27bd13fe6de40c86152a6
sue445 <sue445@sue445.net>
2015-07-27 00:30:51 +0900

    Tweak cli

--- lib/apple_system_status/cli.rb
+++ lib/apple_system_status/cli.rb
@@ -4,7 +4,7 @@ require "apple_system_status"
 module AppleSystemStatus
   class CLI < Thor
     desc "fetch", "Fetch apple system status"
-    option :country, desc: "country code. (ex. jp, ca, fr)"
+    option :country, desc: "country code. (ex. jp, ca, fr)", default: "us"
     option :format, desc: "output format. (ex. plain, json)", default: "plain"
     def fetch
       response = AppleSystemStatus::Crawler.new.perform(options[:country])
