50dc82b22e399d5b90c7e0f13768d55e6681409a
sue445 <sue445@sue445.net>
2018-06-30 23:18:14 +0900

    Tweak spec

--- spec/apple_system_status/cli_spec.rb
+++ spec/apple_system_status/cli_spec.rb
@@ -2,14 +2,14 @@ describe AppleSystemStatus::CLI do
   describe "#fetch" do
     it "should execute" do
       result = system("./exe/apple_system_status fetch")
-      expect(result).to be true
+      expect(result).to eq true
     end
   end
 
   describe "#cli" do
     it "should execute" do
       result = system("./exe/apple_system_status version")
-      expect(result).to be true
+      expect(result).to eq true
     end
   end
 end
